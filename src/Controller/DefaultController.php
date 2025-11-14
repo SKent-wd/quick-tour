35396dac0e684a7276b9d711f26cbb8489d147f1
s-kent <spkent@hotmail.com>
2025-11-14 16:28:14 +0000

    Added Twig templates, Profiler, and Rich API support.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,21 +2,42 @@
 // src/Controller/DefaultController.php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
     #[Route('/hello/{name}', name: 'index')]
     public function index(string $name): Response
     {
-        return new Response("Hello $name!");
+        return $this->render('default/index.html.twig', [
+			'name' => $name,
+		]);
     }
 	
 	#[Route('/simplicity', methods: ['GET'])]
     public function simple(): Response
     {
         return new Response('Simple! Easy! Great!');
+    }
+	
+	#[Route('/test', methods: ['GET'])]
+    public function test(): Response
+    {
+        $testphrase1 = 'The quick brown fox jumps over the lazy dog.';
+		return $this->render('default/test.html.twig', [
+			'testphrase1' => $testphrase1,
+		]);
+    }
+	
+	#[Route('/api/hello/{name}', methods: ['GET'])]
+    public function apiHello(string $name): JsonResponse
+    {
+        return $this->json([
+            'name' => $name,
+            'symfony' => 'rocks',
+        ]);
     }
 }
 ?>
